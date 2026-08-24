5beeab9efaed8bf85091f7378ba225ed21076211
Narayan Guptha <narayanagupthaborra@gmail.com>
2026-08-24 22:09:12 +0530

    day01 completed

---
+++ 01_folder/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1> hello</h1>
+      <h2>hello</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro quo alias unde, magnam cum error tenetur, quas omnis vero fugiat eum ea sunt, similique consequatur dolorum quaerat sed recusandae assumenda?</p>
+    </>
+  )
+}
+
+export default App
