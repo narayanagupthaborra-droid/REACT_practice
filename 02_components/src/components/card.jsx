5c248ff9d904945f5859441b6e421f013e941678
narayanaguptha <narayanagupthaborra@gmail.com>
2026-08-26 20:46:13 +0530

    day 02 components completed

---
+++ 02_components/src/components/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const card = () => {
+  return (
+   <div className="card">
+      <h1>Guptha</h1>
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, aut!</p>
+    </div>
+  )
+}
+
+export default card
---
+++ 02_components/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      <div className="navbar">
+        <h1>Narbar</h1>
+        <p> hi ,iam navbar</p>
+      </div>
+    </div>
+  )
+}
+
+export default navbar;
