2b1199b616d0d86a1ea88a7b7fffc795933d8ca2
narayanaguptha <narayanagupthaborra@gmail.com>
2026-09-03 14:35:33 +0530

    done

---
+++ 13-notesapp-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+     <form className='flex iteams-center'>
+     <input type="text" /><input type="text" />
+     </form>
+
+    </div>
+  )
+}
+
+export default App
